703557efc5763829e8867c8b6e7c9f81e523deba
mcoca <54874341+SlowpokeBloke@users.noreply.github.com>
2024-10-05 17:12:11 -0400

    Delete pepper/src/config directory

--- pepper/src/config/genkit.js
+++
@@ -1,6 +0,0 @@
-import {configureGenkit} from "@genkit-ai/core";
-import {firebase} from "@genkit-ai/firebase";
-
-configureGenkit({
-  plugins: [firebase({projectId: "robotics-project-team-1"})],
-});
